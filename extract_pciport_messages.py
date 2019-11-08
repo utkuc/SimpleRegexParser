753bdea74bf18b065ed385d1a9e64f4080784bb2
Utku Cuhadaroglu <utku.cuhadaroglu@gmail.com>
2019-11-09 00:02:15 +0300

    Added PciPort extraction

--- extract_pciport_messages.py
+++ extract_pciport_messages.py
@@ -14,16 +14,28 @@ def main():
         print("File path {} does not exist. Give valid file path as parameter .(eg: testFile.txt )".format(fileName))
         sys.exit(0)
     extractPciPort(fileName)
-
+#Extract pci port with relative time and description
 def extractPciPort(fileName):
     with open(fileName + "_pciport_messages.txt", "a") as outputFile:
         with open(fileName) as file:
             currentLine = file.readline()
-
-
-
-
-
+            while currentLine:
+                pciport = re.search("([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]:)"
+                                    "([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]"
+                                    ":"
+                                    "[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]"
+                                    "[.]"
+                                    "[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]|"
+                                    "[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F][.][0-9a-fA-F])", currentLine)
+                if pciport:
+                    date = re.search(r"\b(0[  0-9]|1[0-9]|2[0-4])(:)([0-5][0-9])(:)[0-5][0-9]\b", currentLine)
+                    if date:
+                        description = re.search("(?<=kernel:\s).*$", currentLine)
+                        if description:
+                            outputFile.write(str(date[0]).replace(" ", "") + "\t")
+                            outputFile.write(str(pciport[0]) + "\t")
+                            outputFile.write(str(description[0]) + "\n")
+                currentLine = file.readline()
 
 
 if __name__ == '__main__':
